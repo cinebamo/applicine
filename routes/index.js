7de90eb4040c34a7fd71e8d7824fb194c79bef96
ISAsxm <huguesisabelle82@gmail.com>
2019-04-10 14:57:29 +0000

    routes users UPDATE/DELETE LOGIN - ISA

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,23 @@ router.get('/home', function(req, res, next) {
   res.send('ok');
 });
 
+<<<<<<< Updated upstream
 /* Mathias ===> cinebat.dev/movie/$id en GET */
 router.get('/movie/:id', function(req, res, next) {
   res.send('ok ' + req.params.id);
 });
 
+=======
+
+/* 
+*@author isa 
+*/
+/* users  login account  >>cinebat.dev/login */
+router.post('/login', function(req, res, next) {
+
+  res.json('connexion ok'+req.body.id);
+});
+
+
+>>>>>>> Stashed changes
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* 
+*@author isa 
+*/
+/* POST users create account >>cinebat.dev/user*/
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('le compte a bien été crée');
+});
+
+/* 
+*@author isa 
+*/
+/* PUT users update account >>cinebat.dev/user/$id	*/
+router.put('/', function(req, res, next) {
+  // console.log(req.body.id);
+  res.json('le compte a bien été modifié'+req.body.id);
+});
+
+/* 
+*@author isa 
+*/
+/* DELETE users delete account  >>cinebat.dev/user/$id */
+router.delete('/', function(req, res, next) {
+  // console.log(req.body.id);
+  res.json('le compte a bien été supprimé'+req.body.id);
 });
 
 module.exports = router;
