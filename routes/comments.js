cc856cd47007a0d444eb73dfaed8e61b344f0f48
GiorgioSH <sharmazanashvili.georges@outlook.fr>
2019-04-11 16:39:49 +0200

    //Georges - PUT / DELETE LOIIIIIN D ETRE FINI.....

--- routes/comments.js
+++ routes/comments.js
@@ -17,10 +17,33 @@ router.get('/', function(req, res, next) {
 */
 /*POST COMMENTS - Georges */
 /*Update*/
-router.put('/', function(req, res, next) {
-  res.json('Votre commentaire a bien été mis à jour' + req.body.id);
-});
 
+//
+router.put('/', function(req, res, next) {
+  
+  var requiredProps = ['_id', 'name', 'firstname','email'];
+  
+  for(var i in requiredProps[i]) {
+   
+    if(typeof req.body[requiredProps[i]] == 'undefined'){
+      return res.send(requiredProps[i] + 'empty');
+    }
+  }
+  
+  DB.collection('comments').updateOne({_id:ObjectId(req.body)},{name:req.body.name}, function(err, result){
+    
+    if(err) throw err;
+
+      res.json({
+        result : 'ok',
+        id : result.insertedId.toString()
+      });
+
+  });
+
+
+});
+//
 /*Delete*/
 router.delete('/', function (req,res,next){
     res.json('votre commenraire a bien été supprimé' + req.body.id);
