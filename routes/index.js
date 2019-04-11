8858710fd1775632f5cc88f14a56e2a6c716badf
Cinebamo <e.rebeyrolle@it-students.fr>
2019-04-11 14:36:17 +0000

    Max login pour Isa

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,14 @@ var MongoClient = require('mongodb').MongoClient,
 var ObjectId = require('mongodb').ObjectId,
   url= "mongodb://localhost:27017/cinebamo";
 
+  MongoClient.connect(url,
+    {useNewUrlParser:true},
+    function(err, client){
+      if(err) throw err;
+
+      var DB = client.db('cinebamo');
+
+      console.log('je suis connecté');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -35,12 +43,31 @@ router.get('/movie/:id', function(req, res, next) {
 */
 /* users  login account  >>cinebat.dev/login */
 router.post('/login', function(req, res, next) {
-  res.json('connexion ok'+req.body.id);
+
+  var requiredProps = ['email','password'];
+  for(var i in requiredProps[i]) {
+    if(typeof req.body[requiredProps[i]] == 'undefined'){
+      return res.send(requiredProps[i] + 'empty');
+    }
+  }
+  DB.collection('users').insertOne(req.body, function(err, result){
+    
+    if(err) throw err;
+
+      res.json({
+        result : 'ok',
+        id : result.insertedId.toString()
+      });
+
+  });
+
+
 });
+
 
 /* GET CGU page. */
 router.get('/cgu', function(req, res, next) {
   res.render('cgu', {}); 
 });
-
+});
 module.exports = router;
