362cd09a0351edf3ac5fad06a1be50da579135a9
ISAsxm <huguesisabelle82@gmail.com>
2019-04-12 09:22:36 +0000

    update delete users ok

--- routes/users.js
+++ routes/users.js
@@ -52,33 +52,20 @@ router.get('/:id', function(req, res, next) {
 *@author isa 
 */
 /* PUT users UPDATE account >>cinebat.dev/user/$id	*/
-  router.put('/', function(req, res, next) {
+  router.put('/:id', function(req, res, next) {
     
-    var requiredProps = ['name','firstname', 'email', 'password','age'];
-    // je verifie qu'il y ai bien des données reçues en post
-    for(var i in requiredProps[i]) {
-      // si les données reçues sont indefinies répond que le champ est vide et coupe le script avec le return
-      if(typeof req.body[requiredProps[i]] == 'undefined'){
-        return res.send(requiredProps[i] + 'empty');
-      }
-    }
     // si les données reçues ne sont pas indefinis alors tu les update dans la DB et tu affiche ok + toutes les données corespondant a l'id
     DB.collection('users').updateOne(
-        {_id: ObjectId(req.body.id)},
-              {$set:{name:req.body.name,
-                    firstname: req.body.firstname,
-                    email: req.body.email,
-                    password: req.body.password,
-                    age: req.body.age}},
-               
+        {_id: ObjectId(req.params.id)},
+              {$set:req.body},  
         function(err, result){
-    
           if(err) throw err;
+          
             res.json({
             result : 'Votre compte à bien été modifié',
-            id : result.upsertedId.toString()
+            
             });
-          console.log(result.upsertedId.toString());
+            console.log(res.body);
         });
 });
 /**
@@ -114,10 +101,18 @@ router.post('/', function(req, res, next) {
 *@author isa 
 */
 /* DELETE users delete account  >>cinebat.dev/user/$id */
-router.delete('/id', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
   // console.log(req.body.id);
-  res.json('le compte a bien été supprimé'+req.body.id);
-});
+    // recupere lutilisateur dont lid est passe en parametre dans la db
+    DB.collection('users').deleteOne({_id: ObjectId(req.params.id)},function(err, user){
+      // s'il n'y a pas d'erreur
+      if(err) throw err;
 
+      res.send('le compte a bien été supprimé');
+      
+    });
+  
+
+});
 });
 module.exports = router;
