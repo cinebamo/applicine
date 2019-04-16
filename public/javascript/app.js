457deb81ade3a0ad5e8b40768fbf2340e7462889
Aina-ITAkademy <aa.naryandriambelo@it-students.fr>
2019-04-16 11:41:00 +0000

    Modification des cards film: la carte modele est invisible au debut

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -18,59 +18,64 @@ $('#categs').on('change',function(evt){
 /**
  * @author Aina, Georges
  */
-function clonageCard(id, video, title, summary, score) {
+$(document).ready(function(){
+  //Fonction pour cloner la carte modele "card_original"
+  function clonageCard(id, video, title, summary, score) {
 
-  //Info en reponse
-  var cardID = 'card_' + id
-  var cardIDselector = '#' + cardID
-  //Recuperer la carte exemple "cardModel"
-  var cardClone = $("#card_original").clone().attr('id', cardID);
-  //$(cardClone).find("h5").text("exemple");
-  $("#cardRow").append(cardClone)
+    //Info en reponse
+    var cardID = 'card_' + id
+    var cardIDselector = '#' + cardID
+    //Recuperer la carte exemple "cardModel"
+    var cardClone = $("#card_original").clone().attr('id', cardID);
+    //$(cardClone).find("h5").text("exemple");
+    $("#cardRow").append(cardClone)
 
-  //Charger le contenu dans la carte clone
-  var selectorTitle = cardIDselector + " h3"
-  var selectorText = cardIDselector + " p"
-  $(selectorTitle).text(title)
-  $(selectorText).text(summary)
-  $(cardIDselector + " button:first").click(function() {
-    window.location.href = 'film/'+id;
-  })
-  /////////////////////////////////
-  // Todo : les notes et la video
-  //
-  //
-  //Enlever le display none
-  // $(cardIDselector).removeClass("d-none")
-}
+    //Charger le contenu dans la carte clone
+    var selectorTitle = cardIDselector + " h3"
+    var selectorText = cardIDselector + " p"
+    
+    $(selectorTitle).text(title)
+    $(selectorText).text(summary)
+    $(cardIDselector + " button:first").click(function() {
+      window.location.href = 'film/'+id;
+    })
+    $(cardIDselector).css('display', 'initial');
+    /////////////////////////////////
+    // Todo : les notes et la video
+    //
+    //
+    //Enlever le display none
+    // $(cardIDselector).removeClass("d-none")
+  }
 
-// "searchForm" est l'ID du form
-// "Search_title_actor" est l'ID du input
-// "categs" est l'ID du select
-$("#searchForm").submit(function (evt) {
-  evt.stopPropagation()
-  evt.preventDefault()
+  //Fonction appellee quand on fait une recherche
+  //    "searchForm" est l'ID du form
+  //    "Search_title_actor" est l'ID du input
+  //    "categs" est l'ID du select
+  $("#searchForm").submit(function (evt) {
+    evt.stopPropagation()
+    evt.preventDefault()
 
-  $.get({
-    url: "search/",
-    data: {
-      title_actor: $("#Search_title_actor").val(),
-      category: $("#categs").val()
-    },
-    success: function (reponse) {
+    $.get({
+      url: "search/",
+      data: {
+        title_actor: $("#Search_title_actor").val(),
+        category: $("#categs").val()
+      },
+      success: function (reponse) {
 
-      //Traiter chaque carte
+        //Traiter chaque carte
 
-      $("#cardRow > div:not(:first) ").remove()
-      reponse.forEach(function (film) {
-        clonageCard(film['_id'], film['video'], film['title'], film['summary'], film['score'])
-      });
-    },
-    dataType: "json"
-  })
-});
+        $("#cardRow > div:not(:first) ").remove()
+        reponse.forEach(function (film) {
+          clonageCard(film['_id'], film['video'], film['title'], film['summary'], film['score'])
+        });
+      },
+      dataType: "json"
+    })
+  });
 
-
+})
 
 
 
